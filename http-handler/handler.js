7614451f21bd98eddd2f3b5a630fef0d9d2f6344
David Vasconcelos <david.vasconcelos@platformbuilders.io>
2020-09-28 16:33:42 -0300

    - servico do dynamodb; - offline e serverless; - configuracao do debug do vs code;

--- http-handler/handler.js
+++ http-handler/handler.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const s3Service = require('./service/s3Service')
+const dynamoDBService = require('./service/dynamoDBService')
 
 module.exports.upload = async event => {
-  const item = await s3Service.upload(event.body)
+  const item = await s3Service.upload(event.body);
+  await dynamoDBService.put(item);
+
+
   return {
     statusCode: 201,
     body: JSON.stringify(item),
