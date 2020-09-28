f25969693a2282b239b200db08db7284d9b104b5
David Vasconcelos <david.vasconcelos@platformbuilders.io>
2020-09-28 20:01:48 -0300

    - adicionado o bucket de thumbnail; - utilizado o jimp para miniaturizar a imagem;

--- thumbnail-handler/handler.js
+++ thumbnail-handler/handler.js
@@ -1,13 +1,10 @@
 'use strict';
 
+const thumbnailService = require('./service/thumbnailService');
+
 module.exports.thumbnail = async event => {
   console.log('Evento do SNS recebido com sucesso', JSON.stringify(event));
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      }),
-  };  
+  await thumbnailService.thumbnail(event);
+
+  return { message: 'Thumbnail gerado com sucesso', event};
 };
---
+++ thumbnail-handler/service/s3Service.js
@@ -0,0 +1,48 @@
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+    region: 'sa-east-1'
+});
+
+const S3 = new AWS.S3();
+
+const BUCKET = 'nanoservices-imagens-thumbnail';
+
+const getObject = (bucket, key) => {
+    return new Promise((res, rej) => {
+        S3.getObject({
+            Bucket : bucket,
+            Key: key
+        }, (err, data) => {
+            if (err) {
+                return rej(err);
+            }
+            return res(data.Body);
+        });
+
+    });
+}
+
+const putObject = (buffer, fileName) => {
+    return new Promise((res, rej) => {
+        S3.putObject({
+            Bucket : BUCKET,
+            Key: 'thumbnail-' + fileName,
+            Body: buffer
+        }, (err, data) => {
+            if (err) {
+                return rej(err);
+            }
+            return res(data);
+        });
+
+    });
+}
+
+
+
+
+module.exports = {
+    getObject: getObject,
+    putObject: putObject
+}
