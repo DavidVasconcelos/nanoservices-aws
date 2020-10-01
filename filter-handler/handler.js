d919ac7d94e1acff819e832f0d9e1b719739d651
David Vasconcelos <david.vasconcelos@platformbuilders.io>
2020-10-01 19:18:00 -0300

    - criacao filter;

---
+++ filter-handler/handler.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const filterService = require('./service/filterService');
+
+module.exports.filter = async event => {
+  console.log('Evento do SNS recebido com sucesso', JSON.stringify(event));
+  await filterService.filter(event);
+
+  return { message: 'Filter gerado com sucesso', event};  
+};
