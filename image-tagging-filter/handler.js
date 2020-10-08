26558e2293b3bb0c9354cad67d9c355e8fe0588e
David Vasconcelos <david.vasconcelos@platformbuilders.io>
2020-10-08 18:24:44 -0300

    - Enviando tag para o SQS;

--- image-tagging-filter/handler.js
+++ image-tagging-filter/handler.js
@@ -1,41 +1,22 @@
 'use strict';
 
-const AWS = require('aws-sdk');
-
-AWS.config.update({
-  region: 'us-east-1'
-});
-
-const rekognition = new AWS.Rekognition();
+const rekognitionService = require('./service/rekognitionService');
+const sqsService = require('./service/sqsService');
 
 module.exports.tag = async event => {
   const s3Info = JSON.parse(event.Records[0].Sns.Message);
    
   const bucket = s3Info.Records[0].s3.bucket.name;
-  const key = s3Info.Records[0].s3.object.key
+  const key = s3Info.Records[0].s3.object.key;
 
-  const data = await (new Promise((res, rej) => {
+  const labels = await rekognitionService.detectLabels(bucket, key);
 
-    rekognition.detectLabels({
-      Image : {
-        S3Object : {
-          Bucket: bucket,
-          Name: key
-        }      
-      },
-      MinConfidence: 80,
-      MaxLabels: 6
-    }, (err, data) => {
-      
-      if(err) {
-        return (rej(err));
-      }      
-      
-      return res(data);  
-    })
-  }));
-  
-  console.log(data.Labels);
+  const item = {};
+  item.key = key;
+  item.labels = labels;
+  item.eventType = "TAG_EVENT";
+
+  await sqsService.putMessage(item);    
 
   return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
--- thumbnail-handler/service/thumbnailService.js
+++ thumbnail-handler/service/thumbnailService.js
@@ -6,7 +6,7 @@ const thumbnail = async event => {
     const s3Info = JSON.parse(event.Records[0].Sns.Message);
    
     const bucket = s3Info.Records[0].s3.bucket.name;
-    const key = s3Info.Records[0].s3.object.key
+    const key = s3Info.Records[0].s3.object.key;
    
     const s3Object = await s3Service.getObject(bucket, key);
 
