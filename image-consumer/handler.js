814d5f755d099d1aeb084d229bf6f013caac8b00
David Vasconcelos <david.vasconcelos@platformbuilders.io>
2020-10-09 18:56:50 -0300

    - image-consumer final;

--- image-consumer/handler.js
+++ image-consumer/handler.js
@@ -1,57 +1,37 @@
 'use strict';
 
-const AWS = require('aws-sdk');
-
-AWS.config.update({
-  region: 'us-east-1'
-});
-
-const dynamo = new AWS.DynamoDB.DocumentClient();
-
-const elasticsearch = require('elasticsearch');
-
-const client = new elasticsearch.Client({
-  host: 'https://vpc-elasticsearch-cluster-4qiv4s2bcahsiwbyvft5eik23a.us-east-1.es.amazonaws.com',  
-  apiVersion: '7.7',
-});
+const elasticsearchService = require('./service/elasticsearchService');
+const dynamoDBService = require('./service/dynamoDBService');
 
 module.exports.consumer = async event => {
 
-  try {
-    await client.search({
-      index: 'index',
-      q: '*'
-    })
-  } catch (error) {
-    console.log(error);
+  for (const record of event.Records) {
+    const item = JSON.parse(record.body);
+    const dbItem = await dynamoDBService.getItem(item.key);
+
+    switch (item.eventType) {
+      case 'TAG_EVENT':
+        await elasticsearchService.index({
+          id: item.index,
+          tags: item.labels
+        })
+        break;
+      case 'FILTER_EVENT':
+        dbItem.blackWhiteFilter = {
+          bucket: item.bucket,
+          key: item.key
+        };
+        break;
+      case 'THUMBNAIL_EVENT':
+        dbItem.thumbanail = {
+          bucket: item.bucket,
+          key: item.key
+        };
+        break;     
+    }
+    await dynamoDBService.putItem(dbItem);
+
   }
 
-  await (new Promise((resolve, reject) => {
-    dynamo.get({
-      TableName: 'imagens',
-      Key: {
-        id: '123'
-      }
-    }, (err, data) => {
-      console.log('err', err);
-      console.log('data', data);
-
-      return resolve({});
-    });
-  }));
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  return { message: 'Mensagens consumidas com sucesso!', event };
 };
